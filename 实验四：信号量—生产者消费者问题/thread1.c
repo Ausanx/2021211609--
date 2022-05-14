e2107ad2fc7a2a206797dd37db555a7c9a8cc744
Ausanx <1779198095@qq.com>
2022-05-14 08:56:43 +0800

    实验四生产者线程

--- 实验四：信号量—生产者消费者问题/thread1.c
+++ 实验四：信号量—生产者消费者问题/thread1.c
@@ -12,3 +12,39 @@ static rt_thread_t producer_tid = RT_NULL;
 static rt_thread_t consumer_tid = RT_NULL;
 struct rt_semaphore sem_lock;
 struct rt_semaphore sem_empty, sem_full;
+/* 生 产 者 线 程 入 口 */
+void producer_thread_entry(void *parameter) {
+int cnt = 0;
+/* 运 行 10 次 */
+while (cnt < 10)
+{
+/* 获 取 一 个 空 位 */
+rt_sem_take(&sem_empty, RT_WAITING_FOREVER);
+/* 修 改 array 内 容， 上 锁 */
+rt_sem_take(&sem_lock, RT_WAITING_FOREVER);
+array[set % MAXSEM] = cnt + 1;
+rt_kprintf("the producer generates a number: %d\n", array[set % MAXSEM]);
+set++;
+rt_sem_release(&sem_lock);
+/* 发 布 一 个 满 位 */
+rt_sem_release(&sem_full);
+cnt++;
+/* 暂 停 一 段 时 间 */
+rt_thread_mdelay(20);
+}
+rt_kprintf("the producer exit!\n");
+}
+int producer_consumer(void) {
+set = 0;
+get = 0;
+/* 初 始 化 3 个 信 号 量 */
+rt_sem_init(&sem_lock, "lock", 1, RT_IPC_FLAG_FIFO);
+rt_sem_init(&sem_empty, "empty", MAXSEM, RT_IPC_FLAG_FIFO);
+rt_sem_init(&sem_full, "full", 0, RT_IPC_FLAG_FIFO);
+/* 创 建 生 产 者 线 程 */
+producer_tid = rt_thread_create("producer",producer_thread_entry, RT_NULL,
+THREAD_STACK_SIZE,
+THREAD_PRIORITY - 1, THREAD_TIMESLICE);
+if (producer_tid != RT_NULL)
+rt_thread_startup(producer_tid);}
+MSH_CMD_EXPORT(producer_consumer, producer_consumer sample);
